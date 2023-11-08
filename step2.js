8e003eaa09b75de7eee0555659cb8b428ddd5385
paganrage333 <aragan@unca.edu>
2023-11-08 10:54:19 -0500

    node exercises

---
+++ step2.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios')
+
+function cat(path) {
+    fs.readFile(path, 'utf8', function(err,data) {
+        if (err) {
+            console.log("Error:", err);
+            process.exit(1)
+        } else {
+            console.log(data)
+        }
+    })
+}
+
+async function webCat(url) {
+    try {
+        let response = await axios.get(url);
+        console.log(response.data);
+    } catch (err) {
+        console.log("Error:", err)
+        process.exit(1)
+    }
+}
+
+let path = process.argv[2]
+
